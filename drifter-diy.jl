a56a4fd1a184d09e87292240299b67e585e42bff
Alexander Barth <barth.alexander@gmail.com>
2023-02-08 13:39:16 +0100

    outbox

--- drifter-diy.jl
+++ drifter-diy.jl
@@ -55,28 +55,11 @@ end
 
 
 @info "send first message"
+
+outbox_messages = []
+
 message = "$hostname ready, switching GNSS on"
-
-ntry_message_send = 10
-
-for i = 1:ntry_message_send
-    try
-        send_message(sp,phone_number,local_SMS_service_center,message)
-        break
-    catch err
-        @info "catched error (try $i) " err
-
-        reg_status = cmd(sp,"AT+CREG?")
-        @info "registration status" reg_status
-
-        if i == ntry_message_send
-            @info("sending message failed after $ntry_message_send")
-            rethrow()
-        else
-            sleep(60)
-        end
-    end
-end
+push!(outbox_messages,message)
 
 last_message = DateTime(1,1,1)
 last_save = DateTime(1,1,1)
@@ -84,68 +67,100 @@ fname = expanduser("~/track-$hostname-$(Dates.format(Dates.now(),"yyyymmddTHHMMS
 isfile(fname) && rm(fname)
 dt_message = Dates.Second(config["message_every_seconds"])
 dt_save = Dates.Second(config["save_every_seconds"])
+dt_pending_tasks = Dates.Second(10)
 
 @info "saving location every $dt_save in $fname"
 @info "sending location every $dt_message to $phone_number"
 
 gnss_fix = false
 gnss_retry = 0
+sms_retry = 0
 time = longitude = latitude = nothing
+
 open(fname,"a+") do f
     while true
-        global last_message, last_save, gnss_retry, gnss_fix
+        global last_message, last_save, gnss_retry, sms_retry, gnss_fix
         global time, longitude, latitude
+        global outbox_messages
 
         now = Dates.now()
 
         # GNSS coordinates
-        while true
-            time,longitude,latitude = get_gnss(sp)
+        time,longitude,latitude = GSMHat.get_gnss(sp)
 
-            if !isnothing(time) && !isnothing(latitude) && !isnothing(longitude)
-                gnss_fix = true
-                gnss_retry = 0
-                break
-            else
-                gnss_retry = gnss_retry+1
+        if !isnothing(time) && !isnothing(latitude) && !isnothing(longitude)
+            gnss_fix = true
+            gnss_retry = 0
+        else
+            gnss_retry = gnss_retry+1
+        end
+
+        # log position if available
+        if !isnothing(time) && !isnothing(latitude) && !isnothing(longitude)
+            if now - last_message >  dt_message
+                message = "sigo vivo, estoy en $longitude, $latitude, $time"
+                push!(outbox_messages,message)
+                last_message = now
             end
 
-            if gnss_fix && (gnss_retry > 60)
-                GSMHat.reset(sp)
-                GSMHat.unlook(sp,pin)
-                # echo state remains off
-                gnss_retry = 0
+            if now - last_save >  dt_save
+                println(f,time,",",longitude,",",latitude)
+                flush(f)
+                last_save = now
             end
-            sleep(10)
         end
 
-
-        if now - last_message >  dt_message
-            message = "sigo vivo, estoy en $longitude, $latitude, $time"
-            @info "sending: $message"
-            send_message(sp,phone_number,local_SMS_service_center,message)
-            last_message = now
-        end
-
-        if now - last_save >  dt_save
-            println(f,time,",",longitude,",",latitude)
-            flush(f)
-            last_save = now
-        end
-
+        # get all SMS messages
         messages = GSMHat.get_messages(sp)
         @info "$(length(messages)) message(s)"
         for message in messages
             if strip(lowercase(message.sms_message_body)) == "status"
                 msg = "sigo vivo, estoy en $longitude, $latitude, $time"
                 @info "send status $msg"
-                send_message(sp,phone_number,local_SMS_service_center,msg)
-
+                push!(outbox_messages,msg)
                 GSMHat.delete_message(sp, message.index)
             end
         end
-        #sleep(5)
-        sleep(min(dt_save,dt_message))
+
+
+        # send messages from outbox_messages
+        while !isempty(outbox_messages)
+            message = first(outbox_messages)
+            message_send = false
+
+            try
+                @info "sending: $message"
+                send_message(sp,phone_number,local_SMS_service_center,message)
+                message_send = true
+                sms_retry = 0
+                pop!(outbox_messages)
+            catch err
+                @info "catched error (try $i)" err
+                sms_retry = sms_retry+1
+            end
+
+            if !message_send
+                @info "Unable to send message. Will try later again"
+                break
+            end
+        end
+
+        # reset GSM hat if necessary
+        if (gnss_retry > 60) && (sms_retry > 60)
+            GSMHat.reset(sp)
+            GSMHat.unlook(sp,pin)
+            # echo state remains off
+            gnss_retry = 0
+            sms_retry = 0
+        end
+
+        if !isnothing(time) && !isnothing(latitude) && !isnothing(longitude) && isempty(outbox_messages)
+            # OK, all is fine
+            sleep(min(dt_save,dt_message))
+        else
+            # short sleep
+            sleep(min(dt_save,dt_message,dt_pending_tasks))
+        end
     end
 end
 
